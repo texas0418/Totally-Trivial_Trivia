37bc875c40877cec2a076876799d0dca33927cbf
Simon Shih <MyFckngAcct@MacBook-Pro-4.local>
2018-12-22 11:43:30 -0600

    questions and answers load on answer box clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   var questions = [
     {
       question: "What was Ralphie's favorite radio show?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/an-xmas-story.gif",
       choiceA: "The Lone Ranger",
       choiceB: "Little Orphan Annie",
       choiceC: "I Love a Mystery",
@@ -33,7 +33,7 @@ $(document).ready(function() {
     },
     {
       question: "Who did Michael J. Fox replace as the original Marty McFly?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/back-to-the-future.gif",
       choiceA: "Michael Anthony Hall",
       choiceB: "Matthew Broderick",
       choiceC: "Eric Stoltz",
@@ -42,7 +42,7 @@ $(document).ready(function() {
     },
     {
       question: "What is the name of the book that you are given when you die?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/beetlejuice.gif",
       choiceA: "The Handbook for the Recently Deceased",
       choiceB: "Day of the Dead Handbook",
       choiceC: "Rules of the Dead. An Afterlife Rulebook",
@@ -51,7 +51,7 @@ $(document).ready(function() {
     },
     {
       question: "What was on the t-shirt that Axel Foley wore in Beverly Hills Cop?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/beverly-hills-cop.gif",
       choiceA: "Mumford Phys Ed Dept",
       choiceB: "Nike",
       choiceC: "NYPD",
@@ -60,7 +60,7 @@ $(document).ready(function() {
     },
     {
       question: "What is Jack Burton's truck name?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/big-trouble.gif",
       choiceA: "Big Rig",
       choiceB: "Iron Giant",
       choiceC: "Porkchop Express",
@@ -69,7 +69,7 @@ $(document).ready(function() {
     },
     {
       question: "What is the name of the phonebook in the phone booth?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/bill-ted.gif",
       choiceA: "Doing Time",
       choiceB: "Back to the Future",
       choiceC: "Future Tense",
@@ -78,7 +78,7 @@ $(document).ready(function() {
     },
     {
       question: "Brian got an 'F' in shop class because he couldn't make a ceramic what?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/breakfast-club.gif",
       choiceA: "Lion",
       choiceB: "Tiger",
       choiceC: "Bear",
@@ -87,7 +87,7 @@ $(document).ready(function() {
     },
     {
       question: "What candy does Elliott use to lure E.T. out of the backyard shed?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/et.gif",
       choiceA: "Reese’s Pieces",
       choiceB: "Skittles",
       choiceC: "Gummy Bears",
@@ -96,7 +96,7 @@ $(document).ready(function() {
     },
     {
       question: "Which character is incorrectly matched with their occupation?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/fast-times.gif",
       choiceA: "Spiccoli/Drug Dealer",
       choiceB: "Damone/Ticket Scalper",
       choiceC: "Rat/Movie Theater",
@@ -105,7 +105,7 @@ $(document).ready(function() {
     },
     {
       question: "Which of the following was NOT a place visited by Ferris and company?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/ferris-bueller.gif",
       choiceA: "Art Institute of Chicago",
       choiceB: "Chez Quis Restauraunt",
       choiceC: "Chicago White Sox Game",
@@ -114,7 +114,7 @@ $(document).ready(function() {
     },
     {
       question: "Footloose was loosely based on events that took place in which city?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/footloose.gif",
       choiceA: "Perry",
       choiceB: "Elmore City",
       choiceC: "Broken Bow",
@@ -123,7 +123,7 @@ $(document).ready(function() {
     },
     {
       question: "Which future pop star had an uncredited role as ‘birthday girl’ at the young age of 13?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/ghostbusters.gif",
       choiceA: "Debbie Gibson",
       choiceB: "Tiffany",
       choiceC: "Madonna",
@@ -132,7 +132,7 @@ $(document).ready(function() {
     },
     {
       question: "What is the name of the place that Daniel took Ali on their first date?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/karate-kid.gif",
       choiceA: "Blondie's Water Park",
       choiceB: "Putt Putt Golf",
       choiceC: "Golf and Stuff",
@@ -141,7 +141,7 @@ $(document).ready(function() {
     },
     {
       question: "At at the end of the film who becomes the new Dread Pirate Roberts?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/princess-bride.gif",
       choiceA: "Miracle Max",
       choiceB: "Inigo Montoya",
       choiceC: "Fezzik",
@@ -150,7 +150,7 @@ $(document).ready(function() {
     },
     {
       question: "In the movie 'Risky Business', Tom Cruise is seen dancing to this Bob Seger song.",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/risky-business.gif",
       choiceA: "Turn the Page",
       choiceB: "We've Got Tonight",
       choiceC: "Night Moves",
@@ -159,7 +159,7 @@ $(document).ready(function() {
     },
     {
       question: "What is the noise that Scott hears at the hardware store in the beginning of the film?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/teen-wolf.gif",
       choiceA: "A Kid Blowing A Dog Whistle",
       choiceB: "A Loud Boom Box",
       choiceC: "A Wolf Howl",
@@ -168,7 +168,7 @@ $(document).ready(function() {
     },
     {
       question: "What candy bar does Chunk get for riding up the hill?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/goonies.gif",
       choiceA: "Baby Ruth",
       choiceB: "Three Musketeers",
       choiceC: "Snickers",
@@ -177,7 +177,7 @@ $(document).ready(function() {
     },
     {
       question: "Where do the Frog Brothers work?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/lost-boys.gif",
       choiceA: "Pizza Place",
       choiceB: "Record Store",
       choiceC: "Comic Book Store",
@@ -186,7 +186,7 @@ $(document).ready(function() {
     },
     {
       question: "What was the hooker’s name (Jamie Lee Curtis character)?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/trading-places.gif",
       choiceA: "Penelope",
       choiceB: "Amber",
       choiceC: "Ophelia",
@@ -195,7 +195,7 @@ $(document).ready(function() {
     },
     {
       question: "What type of computer do Wyatt and Gary use to design and build Lisa?",
-      imgSrc: "",
+      imgSrc: "assets/images/gifs/weird-science.gif",
       choiceA: "Apple III",
       choiceB: "Memotech MXT512",
       choiceC: "Commodore VIC-20",
@@ -204,6 +204,7 @@ $(document).ready(function() {
     },
   ];
   var backgroundCount = 0;
+  var questionsCount = 0;
   var audioFiveSecLeft = new Audio("assets/audio/time-bomb.mp3");
   var audioCorrect = new Audio("assets/audio/hooray.mp3");
   var audioWrong = new Audio("assets/audio/wrong.mp3");
@@ -225,19 +226,25 @@ $(document).ready(function() {
       "url(" + backgroundImage[backgroundCount] + ")"
     );
   });
-  // changes background image on click of answers div area
+  // changes background image, questions and answers on click of answers div area
   $(".textBoxAnswer").click(function() {
-    backgroundCount++;
     if (backgroundCount > backgroundImage.length - 1) backgroundCount = 0;
+    backgroundCount++;
     $("body").css(
       "background-image",
-      "url(" + backgroundImage[backgroundCount] + ")"
-    );
+      "url(" + backgroundImage[backgroundCount] + ")");
+    if (questionsCount > questions.length - 1) questionsCount = 0;
+    questionsCount++;
+    $("#question").html(questions[questionsCount].question);
+    $("#A").html(questions[questionsCount].choiceA);
+    $("#B").html(questions[questionsCount].choiceB);
+    $("#C").html(questions[questionsCount].choiceC);
+    $("#D").html(questions[questionsCount].choiceD);
     stopTimer();
     resetTimer();
     startTimer();
   });
-// resets timer on each new background and question answer load1
+// resets timer on each new background and question answer load
   function resetTimer() {
     countdownTimer = 30;
     $("#countdownTimer").text("Time Left: 00:30");
@@ -262,6 +269,9 @@ $(document).ready(function() {
       backgroundCount++;
       if (backgroundCount > backgroundImage.length - 1) backgroundCount = 0;
       $("body").css("background-image", "url(" + backgroundImage[backgroundCount] + ")");
+      if (questionsCount > questions.length - 1) questionsCount = 0;
+      questionsCount++;
+      $("#question").html(questions[questionsCount].question);
       stopTimer();
       resetTimer();
       startTimer();
@@ -284,13 +294,13 @@ $(document).ready(function() {
     }
     return minutes + ":" + seconds;
   }
-
+// audio alert that starts when there are 5 seconds left on the timer
   setTimeout(fiveSeconds, 1000 * 26);
 
   function fiveSeconds() {
     audioFiveSecLeft.play();
   }
-
+// on start button click. hide button and shows question and answers on page
   $("#game-start-button").click(function() {
     $("button").hide();
     $(".textBoxAnswer").show();
@@ -301,28 +311,21 @@ $(document).ready(function() {
     startTimer();
   });
 
-  function loadQuestions() {
-    for (var i = 0; i < questions.length; i++){
-      $(imgSrc).hide();
-      $("#question").html(question);
-      $("#A").html(choiceA);
-      $("#B").html(choiceB);
-      $("#C").html(choiceC);
-      $("#D").html(choiceD);
-    }
-  }
-
-// Load question and answers.
 
 
 // When user clicks answer choice, decides if it is correct or wrong.
+// function checkAnswer(){
 
+// }
 
 // If the answer is correct, plays audioCorrect, shows correct, shows factoid, plays a gif. 
-
+// function correctAnswer(){
+// }
 
 // if answer is wrong, plays audioWrong, shows the correct answer and factoid and plays gif
-
+// function wrongAnswer(){
+  
+// }
 
 
 
